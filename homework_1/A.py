34765f60e1b840244b63dcb5cebae537fe21b3f3
cricetgrig <68951757+cricetgrig@users.noreply.github.com>
2021-06-29 14:07:24 +0300

    Update A.py

--- homework_1/A.py
+++ homework_1/A.py
@@ -1,1 +1,9 @@
+tRoom, tCond = input().split(' ')
+tRoom = int(tRoom)
+tCond = int(tCond)
+mode = input()
 
+if (mode == 'fan') or (tRoom > tCond and mode == 'heat') or (tRoom < tCond and mode == 'freeze'):
+    print(tRoom)
+else:
+    print(tCond)
