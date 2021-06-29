5736fa6241f5e2cdb2514254543568d1f976d9df
cricetgrig <68951757+cricetgrig@users.noreply.github.com>
2021-06-29 14:10:39 +0300

    Update G.py

--- homework_1/G.py
+++ homework_1/G.py
@@ -1,1 +1,12 @@
+N, K, M = input().split(' ')
+N = int(N)
+K = int(K)
+M = int(M)
 
+details = 0 
+
+while N >= K and M <= K:
+    details += (N // K) * (K // M)
+    N = (N % K) + (N // K)*(K % M)
+    
+print(details)
