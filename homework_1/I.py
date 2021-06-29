9293a7bfb553ce219ffb4977482b0b8ae088356e
cricetgrig <68951757+cricetgrig@users.noreply.github.com>
2021-06-29 14:11:47 +0300

    Update I.py

--- homework_1/I.py
+++ homework_1/I.py
@@ -1,1 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
 
+l = [a, b, c]
+l = sorted(l)
+
+if (d >= l[0] and e >= l[1]) or (d >= l[1] and e >= l[0]):
+    print('YES')
+else:
+    print('NO')
